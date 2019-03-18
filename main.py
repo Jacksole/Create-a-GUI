33928b5acbf2e29d72ded104e47791b54f5a40df
Le Aundre Jackson <dell.5287@gmail.com>
2019-03-18 16:26:39 -0400

    Modified main file
    Allowed the press button to display in CMD box

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ class MyFrame(wx.Frame):
         self.text_ctrl = wx.TextCtrl(panel)
         my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
         my_btn = wx.Button(panel, label='Press Me')
+        my_btn.Bind(wx.EVT_BUTTON, self.on_press)
         my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
         panel.SetSizer(my_sizer)        
         self.Show()
+
+    def on_press(self, event):
+        value = self.text_ctrl.GetValue()
+        if not value:
+            print("You didn't enter anything!")
+        else:
+            print(f'You typed: "{value}"')
 
 if __name__ == '__main__':
     app = wx.App()
