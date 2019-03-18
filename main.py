8b8b045ec667b1720bc7aefe213a605d6ab30628
Le Aundre Jackson <dell.5287@gmail.com>
2019-03-18 16:39:48 -0400

    Modified python file
    modified it to create a music player

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
+'''An app to create a text box and to display on shell'''
+import eyed3
+import glob
 import wx
 
-class MyFrame(wx.Frame):    
+class Mp3Panel(wx.Panel):
+    '''Create a panel to display the music infor'''    
+    def __init__(self, parent):
+        super().__init__(parent)
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.row_obj_dict = {}
+
+        self.list_ctrl = wx.ListCtrl(
+            self, size=(-1, 100), 
+            style=wx.LC_REPORT | wx.BORDER_SUNKEN
+        )
+        self.list_ctrl.InsertColumn(0, 'Artist', width=140)
+        self.list_ctrl.InsertColumn(1, 'Album', width=140)
+        self.list_ctrl.InsertColumn(2, 'Title', width=200)
+        main_sizer.Add(self.list_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
+        edit_button = wx.Button(self, label='Edit')
+        edit_button.Bind(wx.EVT_BUTTON, self.on_edit)
+        main_sizer.Add(edit_button, 0, wx.ALL | wx.CENTER, 5)        
+        self.SetSizer(main_sizer)
+
+    def on_edit(self, event):
+        print('in on_edit')
+
+    def update_mp3_listing(self, folder_path):
+    '''Displays Folder Path'''
+        print(folder_path)
+        
+class Mp3Frame(wx.Frame):
+    '''creates a GUI frame'''
     def __init__(self):
-        super().__init__(parent=None, title='Hello World')
-        panel = wx.Panel(self)        
-        my_sizer = wx.BoxSizer(wx.VERTICAL)        
-        self.text_ctrl = wx.TextCtrl(panel)
-        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
-        my_btn = wx.Button(panel, label='Press Me')
-        my_btn.Bind(wx.EVT_BUTTON, self.on_press)
-        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
-        panel.SetSizer(my_sizer)        
+        super().__init__(parent=None,
+                         title='Mp3 Tag Editor')
+        self.panel = Mp3Panel(self)
         self.Show()
 
-    def on_press(self, event):
-        value = self.text_ctrl.GetValue()
-        if not value:
-            print("You didn't enter anything!")
-        else:
-            print(f'You typed: "{value}"')
-
 if __name__ == '__main__':
-    app = wx.App()
-    frame = MyFrame()
+    app = wx.App(False)
+    frame = Mp3Frame()
     app.MainLoop()
